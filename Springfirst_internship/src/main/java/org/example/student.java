6d3310c4ece79feb5f8dc0f18efcac6eb3790b91
SamuvelGipson <samgipson302@gmail.com>
2025-07-14 23:00:19 +0530

    Initial commit: added Spring Boot and React code

---
+++ Springfirst_internship/src/main/java/org/example/student.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class student {
+    public student(){
+        System.out.println("Inside student class");
+    }
+    public static void internship(){
+        System.out.println("Students are attending the internship program in Code STUDIO");
+    }
+}
